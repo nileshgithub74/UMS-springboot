9ce80e4333027bcffff2a951f9972a461c8cc766
Nilesh kumar <kumarnilesh843127@gmail.com>
2026-09-11 09:36:59 +0530

    University management Sytstem using Springboot

---
+++ src/test/java/com/nilesh/University/UniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilesh.University;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
